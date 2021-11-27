a62a6a521fa56ac86b47c91bce5f897c7482b035
Alexandru Florea <alexflorea16000@gmail.com>
2021-11-27 18:17:38 +0200

    Multithreaded server client

--- src/main/java/org/pcbe/Main.java
+++ src/main/java/org/pcbe/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Client client = new Client();
-        client.startConnection("127.0.0.1", 6666);
+        client.startConnection("127.0.0.1", 4444);
         String response = client.sendMessage("hello server");
+        System.out.println(response);
+        response = client.sendMessage("this is a message");
+        System.out.println(response);
+        response = client.sendMessage(".");
         System.out.println(response);
     }
 
