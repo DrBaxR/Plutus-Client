635c17d84f001d3fcd59d268d273ac410c096d45
Alexandru Florea <alexflorea16000@gmail.com>
2021-11-27 18:01:20 +0200

    Simple client

--- src/main/java/org/pcbe/Main.java
+++ src/main/java/org/pcbe/Main.java
@@ -1,13 +1,12 @@
 package org.pcbe;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        System.out.println(response);
     }
+
 }
